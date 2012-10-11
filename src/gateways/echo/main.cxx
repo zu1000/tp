706e35ef5cafadf07186f363df69758691ea89f3
yya <yya@yya-virtual-machine.(none)>
2012-10-11 20:36:42 +0200

    return the size and fix some small issue

--- src/gateways/echo/main.cxx
+++ src/gateways/echo/main.cxx
@@ -7,20 +7,25 @@
 
 using namespace tp::comm;
 
+std::vector< boost::shared_ptr<io::peer> > peers;
+
 std::size_t handle_message(const io::const_buffer& buff, std::size_t size)
 {
     std::string str(boost::asio::buffer_cast<const char*>(buff), size);
     std::cout << str << std::endl;
+    return size;
 }
 
 bool handle_error(const io::error_code& error)
 {
+    return true;
 }
 
 void handle_peer(boost::shared_ptr<io::peer> p)
 {
     std::cout << "get new peer: " << p->peer_address() << std::endl;
     p->init_peer(handle_message, handle_error);
+    peers.push_back(p);
 }
 
 int main(void)
